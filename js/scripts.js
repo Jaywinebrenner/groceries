b00d27eac8e25448cc5a03d550bea22bec6172b8
jay thomas winebrenner <jaywinebrenner1@jays-iMac.local>
2019-10-28 20:52:33 -0700

    fix code - up to date and functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,49 @@
-$(document).ready(function() {
-  $(".groceryForm").submit(function(event) {
+
+
+
+
+$(document).ready(function(){
+  $(".groceryForm").submit(function(event){
     event.preventDefault();
 
-    var groceries = ["1", "2", "3"];
-    var groceArray = [];
+    var blanks = ["grocery1", "grocery2", "grocery3"];
+    console.log(blanks);
+    blanks.forEach(function(blank){
+      var userInput = $("#" + blank).val();
+      $("ul").append("<li>" + userInput + "</li>");
 
-    groceries.forEach(function(groce) {
-      var capUserInput = $("#inputGrocery" + groce).val().toUpperCase();
-      groceArray.push(capUserInput);
+    })
+  })
+})
 
-      $(".hide").hide();
-    });
 
-    groceArray.sort();
 
-    groceArray.forEach(function(groc) {
-      $("#outputGrocery").append("<li>" + groc + "</li>");
-    });
-  });
-});
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+  //   $(".groceryForm").submit(function(event) {
+    //     event.preventDefault();
+    //
+    //     var groceries = ["1", "2", "3"];
+    //     var groceArray = [];
+    //
+    //     groceries.forEach(function(groce) {
+      //       var capUserInput = $("#inputGrocery" + groce).val().toUpperCase();
+      //       groceArray.push(capUserInput);
+      //
+      //       $(".hide").hide();
+      //     });
+      //
+      //     groceArray.sort();
+      //
+      //     groceArray.forEach(function(groc) {
+        //       $("#outputGrocery").append("<li>" + groc + "</li>");
+        //     });
+        //   });
+        // });
